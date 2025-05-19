51c46f4d7c82fbfb5ac6f996bfae614961c75ba2
mr4909 <mroberts@csg.org>
2025-05-19 11:30:10 -0400

    add more context to analysis walkthrough

--- R/setup.R
+++ R/setup.R
@@ -42,17 +42,3 @@ required_packages <- c(
 
 # Load packages quietly
 invisible(lapply(required_packages, library, character.only = TRUE))
-
-
-
-
-# https://www.pinterest.com/pin/472878029644220127/
-colors = c("#1b4793", "#2178ae", "#fac92c",
-           "#f7b0be", "#ed8e83", "#f15a42",  "#ef3c23",
-           "#18bc9c")
-
-colors = c("#024555", "#ff5738", "#edba3d", "#95b0dd")
-colors = c("#3d1a8e", "#f393af", "#f66529", "#68c7c9", "#fccd0d", "#f3e5d8")
-colors = c("#d7790f", "#3F8BCA", "#00BDB2", "#F4B811", "#DE663E", "#FF912B",
-           "#1b4793", "#fc7255")
-colors = c("#FF5B22", "#3939ff", "#dbb8ff", "#aee6ed", "#f2bb05")
